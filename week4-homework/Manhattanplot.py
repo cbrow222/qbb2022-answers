d26e72d2b052de6acb71ddce68fb8fcca7dd20b0
Cat <cbrow222@jhu.edu>
2022-10-14 13:04:04 -0400

    I guess I changed something

--- week4-homework/Manhattanplot.py
+++ week4-homework/Manhattanplot.py
@@ -8,25 +8,25 @@ data=np.genfromtxt("plink.assoc.linear", dtype=[int, str, int, str, str, int, fl
 cutoff=5
 y = -np.log10(data["P"][1:])
 x = range(len(y))
-# x1=[]
-# x2=[]
-# y1=[]
-# y2=[]
-# for i, value in enumerate(y):
-#     if value>=5:
-#         x1.append(x[i])
-#         y1.append(value)
-#     else:
-#         x2.append(x[i])
-#         y2.append(value)
-        
-# fig, ax= plt.subplots()
-# ax.scatter(x1, y1)
-# ax.scatter(x2,y2)
-# ax.set_ylabel("-log10(P-value)")
-# ax.set_xlabel("Genomic location")
-# plt.savefig("GS451_Manhattan_plot.png")
-# plt.close(fig)
+x1=[]
+x2=[]
+y1=[]
+y2=[]
+for i, value in enumerate(y):
+    if value>=5:
+        x1.append(x[i])
+        y1.append(value)
+    else:
+        x2.append(x[i])
+        y2.append(value)
+
+fig, ax= plt.subplots()
+ax.scatter(x1, y1)
+ax.scatter(x2,y2)
+ax.set_ylabel("-log10(P-value)")
+ax.set_xlabel("Genomic location")
+plt.savefig("GS451_Manhattan_plot.png")
+plt.close(fig)
 
 info=np.genfromtxt("plink.assoc.linear", dtype=None, encoding=None, names = ["Chr", "SNP", "BP", "A1", "Test", "NMISS", "Beta", "Stat", "P"])
 
